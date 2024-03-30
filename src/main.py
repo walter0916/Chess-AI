e8e39e3834a105007eec20263d58ed44979136b0
WalterGuerra <wsteven00@gmail.com>
2024-03-30 11:36:48 -0400

    add game clas to draw the board and add square class

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 
 from const import *
+from game import Game
 
 class Main: 
 
@@ -9,14 +10,25 @@ class Main:
     pygame.init()
     self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Chess')
+    self.game = Game()
 
   def mainloop(self):
+
+    game = self.game
+    screen = self.screen
     
     while True:
+      game.show_bg(screen)
+
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
           sys.exit()
+
+
+
+
+      pygame.display.update()
 
 main = Main()
 main.mainloop()
