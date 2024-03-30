6e5624d118794ee4cc1c8791a8a7407772001e6f
WalterGuerra <wsteven00@gmail.com>
2024-03-30 16:59:46 -0400

    add design for last move and on hover as well as functionality to change turns when clciked

--- src/main.py
+++ src/main.py
@@ -23,8 +23,11 @@ class Main:
     
     while True:
       game.show_bg(screen)
+      game.show_last_move(screen)
       game.show_moves(screen)
       game.show_pieces(screen)
+
+      game.show_hover(screen)
 
       if dragger.dragging:
         dragger.update_blit(screen)
@@ -41,21 +44,31 @@ class Main:
           # check if clicked square has piece
           if board.squares[clicked_row][clicked_col].has_piece():
             piece = board.squares[clicked_row][clicked_col].piece
-            board.calc_moves(piece, clicked_row, clicked_col)
-            dragger.save_initial(event.pos)
-            dragger.drag_piece(piece)
-            # show methods
-            game.show_bg(screen)
-            game.show_moves(screen)
-            game.show_pieces(screen)
+            # valid piece color?
+            if piece.color == game.next_player:
+              board.calc_moves(piece, clicked_row, clicked_col)
+              dragger.save_initial(event.pos)
+              dragger.drag_piece(piece)
+              # show methods
+              game.show_bg(screen)
+              game.show_last_move(screen)
+              game.show_moves(screen)
+              game.show_pieces(screen)
 
         # mouse motion
         elif event.type == pygame.MOUSEMOTION:
+          motion_row = event.pos[1] // SQSIZE
+          motion_col = event.pos[0] // SQSIZE
+
+          game.set_hover(motion_row, motion_col)
+
           if dragger.dragging:
             dragger.update_mouse(event.pos)
             game.show_bg(screen)
+            game.show_last_move(screen)
             game.show_moves(screen)
             game.show_pieces(screen)
+            game.show_hover(screen)
             dragger.update_blit(screen)
 
         # click release
@@ -77,7 +90,10 @@ class Main:
               board.move(dragger.piece, move)
               # show methods
               game.show_bg(screen)
+              game.show_last_move(screen)
               game.show_pieces(screen)
+              # next turn
+              game.next_turn()
 
           dragger.undrag_piece()
 
