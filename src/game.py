5e6023b14acd9d742433f8049f790f63afb807b9
WalterGuerra <wsteven00@gmail.com>
2024-03-30 12:57:28 -0400

    add functionality to drag a piece when clicked and rest when unclicked

--- src/game.py
+++ src/game.py
@@ -2,11 +2,13 @@ import pygame
 
 from const import *
 from board import Board
+from dragger import Dragger
 
 class Game:
 
   def __init__(self):
     self.board = Board()
+    self.dragger = Dragger()
 
   # Show methods
 
@@ -18,17 +20,20 @@ class Game:
         else:
           color = (119, 154, 88) # dark green
 
-          rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
+        rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
 
-          pygame.draw.rect(surface, color, rect)
+        pygame.draw.rect(surface, color, rect)
   
   def show_pieces(self, surface):
     for row in range(ROWS):
       for col in range(COLS):
         if self.board.squares[row][col].has_piece():
           piece = self.board.squares[row][col].piece
-
-          img = pygame.image.load(piece.texture)
-          img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
-          piece.texture_rect = img.get_rect(center=img_center)
-          surface.blit(img, piece.texture_rect)
+          
+          # all pieces expect dragger piece
+          if piece is not self.dragger.piece:
+            piece.set_texture(size=80)
+            img = pygame.image.load(piece.texture)
+            img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
+            piece.texture_rect = img.get_rect(center=img_center)
+            surface.blit(img, piece.texture_rect)
